622ca2cc55a3e18c94218e6ba50c61fad06b024b
lic <1099959038@qq.com>
2019-09-10 17:20:51 +0800

    0910

---
+++ ant_app/src/main/java/com/ant/app/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.ant.app.dao;
+
+import com.ant.app.Request.LoginRequest;
+import com.ant.app.entity.User;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * UserDao继承基类
+ */
+@Repository
+public interface UserDao extends MyBatisBaseDao<User, Integer> {
+
+    User findUser(LoginRequest user);
+
+    User findUserByInvitationCode(@Param("invitationCode") String invitationCode);
+}
